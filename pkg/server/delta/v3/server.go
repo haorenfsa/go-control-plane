9598abe05c4319ef63895bfca9f777c12cfdf7c8
haorenfsa <haorenfsa@gmail.com>
2024-10-28 22:41:50 +0800

    try fix eds
    Signed-off-by: haorenfsa <haorenfsa@gmail.com>

--- pkg/server/delta/v3/server.go
+++ pkg/server/delta/v3/server.go
@@ -120,6 +120,13 @@ func (s *server) processDelta(str stream.DeltaStream, reqCh <-chan *discovery.De
 		watch := watches.deltaWatches[typ]
 		watch.nonce = nonce
 
+		if typ == resource.EndpointType {
+			watch.state.SetClusterWarming(false)
+		}
+		if typ == resource.ClusterType {
+			watch.state.SetClusterWarming(true)
+		}
+
 		log.Printf("send resp ok: %p type %s nonce %s, set nexVersions: %v\n", resp, typ, nonce, resp.GetNextVersionMap())
 
 		watch.state.SetResourceVersions(resp.GetNextVersionMap())
@@ -204,7 +211,6 @@ func (s *server) processDelta(str stream.DeltaStream, reqCh <-chan *discovery.De
 			}
 
 			typeURL := req.GetTypeUrl()
-
 			// cancel existing watch to (re-)request a newer version
 			watch, ok := watches.deltaWatches[typeURL]
 			if !ok {
